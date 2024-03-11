5f00107d79798cbd60a8ed0627656c7c64fa2538
Marco Antonio <marcotonho38@gmail.com>
2024-03-11 00:08:28 -0300

    STYLE : adicionando estilos nos resultados

--- my-project/src/components/Form/ResultImc/index.js
+++ my-project/src/components/Form/ResultImc/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { View, Text } from "react-native";
+import styles from "./style";
 
-export default function ResultImc(props){
-    return(
-        <View>
-            <Text>{props.messageResultImc}</Text>
-            <Text>{props.resultImc}</Text>
-        </View>
-    );
+export default function ResultImc(props) {
+  return (
+    <View style={styles.resultImc}>
+      <Text style={styles.numerImc}>{props.resultImc}</Text>
+      <Text style={styles.information}>{props.messageResultImc}</Text>
+    </View>
+  );
 }
